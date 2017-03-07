682ba47a23465ae6e15df096f960f6ae5a609473
Dominic <mrdjfraser@gmail.com>
2017-03-07 16:51:09 +0000

    added full months to hash so not cheating the test

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -33,8 +33,17 @@ end
 def number_to_full_month_name(month_int)
   months = {
     1 => "January",
+    2 => "February",
     3 => "March",
-    9 => "September"
+    4 => "April",
+    5 => "May",
+    6 => "June",
+    7 => "July",
+    8 => "August",
+    9 => "September",
+    10 => "October",
+    11 => "November",
+    12 => "December"
   }
 
   return months[month_int]
